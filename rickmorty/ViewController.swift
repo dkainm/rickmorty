69be47624502f2e375d1c7ef718cd2ced54ecb8d
Alexander Rudoi <sanikbrant@gmail.com>
2023-02-10 14:26:55 +0100

    Initial commit

---
+++ rickmorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickmorty
+//
+//  Created by Alex Rudoi on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
